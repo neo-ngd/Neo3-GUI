d514b6bd6561a2b0aa99d237e6d2af6c228d06bd
bokuns <sunbokun@onchain.com>
2020-08-05 10:16:45 +0800

    add Back button on deeper-level pages

--- neo3-gui/neo3-gui/ClientApp/src/components/sync.js
+++ neo3-gui/neo3-gui/ClientApp/src/components/sync.js
@@ -1,10 +1,13 @@
 
 /* eslint-disable */
 import React from 'react';
-import { Typography } from 'antd';
+import { withRouter } from 'react-router-dom';
+import { Typography, Button } from 'antd';
 import 'antd/dist/antd.css';
+
 import {
-  SyncOutlined
+  SyncOutlined, 
+  ArrowLeftOutlined
 } from '@ant-design/icons';
 import { observer, inject } from "mobx-react";
 import { withTranslation } from "react-i18next"
@@ -19,17 +22,39 @@ class Sync extends React.Component {
   render() {
     const { t } = this.props;
     const { syncHeight, headerHeight } = this.props.blockSyncStore;
+    const location = this.props.location.pathname;
+
+    var topBarStyle = {
+      display: 'flex',
+      flexDirection: 'row-reverse',
+      justifyContent: 'space-between',
+      alignItems: 'center'
+    };
+
+    console.log(location);
     return (
-      <div className="ml3 mb0">
-        {
-          headerHeight < 0
-            ? <Text className="t-normal bold"> - / - {t("common.connecting")}</Text>
-            : <Text className="t-normal bold"> {syncHeight} / {headerHeight} {t("common.syncing")}</Text>
-        }
-        <SyncOutlined className="ml3" type="sync" spin />
+      <div className="topBar" style={topBarStyle}>
+        <div className="mr3 mb0">
+          {
+            headerHeight < 0
+              ? <Text className="t-normal bold"> - / - {t("common.connecting")}</Text>
+              : <Text className="t-normal bold"> {syncHeight} / {headerHeight} {t("common.syncing")}</Text>
+          }
+          <SyncOutlined className="ml3" type="sync" spin />
+        </div>
+        <div className="ml3" style={{ display: (location === '/' || location.indexOf(':') < 0 ) ? 'none' : 'default' }}>
+          <Button
+            type="link"
+            style={{ color: '#00AF92', textAlign: 'left' }}
+            icon={<ArrowLeftOutlined />}
+            onClick={this.props.history.goBack}
+          >
+            Back
+          </Button>
+        </div>
       </div>
     );
   }
 }
 
-export default Sync;
+export default withRouter(Sync);
